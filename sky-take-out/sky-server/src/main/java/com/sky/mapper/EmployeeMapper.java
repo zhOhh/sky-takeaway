75ff83815a892776cd4b5761f4b2f2dfb7044bcb
zhOhh <1299410261@qq.com>
2023-11-21 23:26:53 +0800

    公共字段自动填充代码实现

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -27,6 +29,7 @@ public interface EmployeeMapper {
     @Insert("insert into sky_take_out.employee(name,username,password,phone,sex,id_number,status,create_time,update_time,create_user,update_user) " +
             "values" +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{status},#{createTime},#{updateTime},#{createUser},#{updateUser})")
+    @AutoFill(value = OperationType.INSERT)
     void insert(Employee employee);
 
     /**
@@ -42,6 +45,7 @@ public interface EmployeeMapper {
      *
      * @param employee
      */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     /**
@@ -50,6 +54,6 @@ public interface EmployeeMapper {
      * @param id
      * @return
      */
-    @Select("select * from employee where id=#{id}")
+    @Select("select * from sky_take_out.employee where id=#{id}")
     Employee getById(Long id);
 }
