49e957e9fd4caf29960463aae56808c5fb027cfb
zhOhh <1299410261@qq.com>
2023-12-01 17:26:22 +0800

    商品浏览业务功能代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/SetmealService.java
@@ -1,6 +1,10 @@
 package com.sky.service;
 
 import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.vo.DishItemVO;
+
+import java.util.List;
 
 public interface SetmealService {
 
@@ -9,4 +13,18 @@ public interface SetmealService {
      * @param setmealDTO
      */
     void saveWithDish(SetmealDTO setmealDTO);
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    List<Setmeal> list(Setmeal setmeal);
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    List<DishItemVO> getDishItemById(Long id);
 }
--- sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
+++ sky-take-out/sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -6,6 +6,7 @@ import com.sky.entity.SetmealDish;
 import com.sky.mapper.SetmealDishMapper;
 import com.sky.mapper.SetmealMapper;
 import com.sky.service.SetmealService;
+import com.sky.vo.DishItemVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,4 +53,24 @@ public class SetmealServiceImpl implements SetmealService {
         setmealDishMapper.insertBatch(setmealDishes);
 
     }
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    public List<Setmeal> list(Setmeal setmeal) {
+        List<Setmeal> list = setmealMapper.list(setmeal);
+        return list;
+    }
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    public List<DishItemVO> getDishItemById(Long id) {
+        return setmealMapper.getDishItemBySetmealId(id);
+    }
+
 }
