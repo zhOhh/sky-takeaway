1b425926b922191a5c5a7b5feb4078b9a881c7c9
zhOhh <1299410261@qq.com>
2024-01-16 16:27:01 +0800

    缓存套餐代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/SetmealController.java
@@ -7,6 +7,7 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +32,7 @@ public class SetmealController {
      */
     @PostMapping
     @ApiOperation("新增套餐")
+    @CacheEvict(cacheNames = "setmealCache",key = "#setmealCache.categoryId")
     public Result save(@RequestBody SetmealDTO setmealDTO){
         setmealService.saveWithDish(setmealDTO);
         return Result.success();
