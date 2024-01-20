3a9c22592f4274e965a555b2ef508173c9b39319
zhOhh <1299410261@qq.com>
2024-01-20 11:05:24 +0800

    导出运营数据报表业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 /**
@@ -94,5 +95,14 @@ public class ReportController {
     }
 
 
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
+    }
 
 }
